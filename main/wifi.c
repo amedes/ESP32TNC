e6d332969ec573ae6f04a27013731f379a58a45a
amedes <amedes@dff.mydns.jp>
2021-02-23 10:02:31 +0900

    modify to adapt to ESP-IDF v4.2

--- main/wifi.c
+++ main/wifi.c
@@ -43,13 +43,15 @@
 */
 #define EXAMPLE_ESP_WIFI_SSID      CONFIG_ESP_WIFI_SSID
 #define EXAMPLE_ESP_WIFI_PASS      CONFIG_ESP_WIFI_PASSWORD
+#define EXAMPLE_ESP_MAXIMUM_RETRY  10
 
 /* FreeRTOS event group to signal when we are connected*/
 static EventGroupHandle_t s_wifi_event_group;
 
 /* The event group allows multiple bits for each event, but we only care about one event 
  * - are we connected to the AP with an IP? */
-const int WIFI_CONNECTED_BIT = BIT0;
+#define WIFI_CONNECTED_BIT	BIT0
+#define WIFI_FAIL_BIT		BIT1
 
 static const char *TAG = "wifi";
 
@@ -57,64 +59,30 @@ static int s_retry_num = 0;
 
 #define WPS_TEST_MODE WPS_TYPE_PBC
 
-#define PIN2STR(a) (a)[0], (a)[1], (a)[2], (a)[3], (a)[4], (a)[5], (a)[6], (a)[7] 
-#define PINSTR "%c%c%c%c%c%c%c%c"
-
+#ifdef USE_WPS
 static esp_wps_config_t config = WPS_CONFIG_INIT_DEFAULT(WPS_TEST_MODE);
-
-static esp_err_t event_handler(void *ctx, system_event_t *event)
-{
-    switch(event->event_id) {
-    case SYSTEM_EVENT_STA_START:
-        s_retry_num = 0;
-        esp_wifi_connect();
-        break;
-    case SYSTEM_EVENT_STA_GOT_IP:
-        ESP_LOGI(TAG, "got ip:%s",
-                 ip4addr_ntoa(&event->event_info.got_ip.ip_info.ip));
-        s_retry_num = 0;
-        xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
-        break;
-    case SYSTEM_EVENT_STA_DISCONNECTED:
-	ESP_LOGI(TAG, "SYSTEM_EVENT_STA_DISCONNECTED");
-        ESP_LOGI(TAG,"connect to the AP fail");
-	ESP_LOGI(TAG, "ssid: %s", event->event_info.disconnected.ssid);
-        {
-            if (s_retry_num < 10) {
-                ESP_ERROR_CHECK(esp_wifi_connect());
-                xEventGroupClearBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
-                s_retry_num++;
-                ESP_LOGI(TAG,"retry to connect to the AP");
-            } else {
-#if 0
-		ESP_ERROR_CHECK(esp_wifi_wps_enable(&config));
-		ESP_ERROR_CHECK(esp_wifi_wps_start(0));
-		ESP_LOGI(TAG, "retry to wps...");
 #endif
-                s_retry_num = 0;
-	    }
-            break;
-        }
-    case SYSTEM_EVENT_STA_WPS_ER_SUCCESS:
-	ESP_LOGI(TAG, "SYSTEM_EVENT_STA_WPS_ER_SUCCESS");
-	ESP_ERROR_CHECK(esp_wifi_wps_disable());
-        ESP_ERROR_CHECK(esp_wifi_connect());
-	break;
-    case SYSTEM_EVENT_STA_WPS_ER_FAILED:
-    case SYSTEM_EVENT_STA_WPS_ER_TIMEOUT:
-	ESP_LOGI(TAG, "SYSTEM_EVENT_STA_WPS_ER_FAILED/TIMEOUT");
-	ESP_ERROR_CHECK(esp_wifi_wps_disable());
-	ESP_ERROR_CHECK(esp_wifi_wps_enable(&config));
-	ESP_ERROR_CHECK(esp_wifi_wps_start(0));
-	break;
-    case SYSTEM_EVENT_STA_WPS_ER_PIN:
-	ESP_LOGI(TAG, "SYSTEM_EVENT_STA_WPS_ER_PIN");
-	ESP_LOGI(TAG, "WPS_PIN = "PINSTR, PIN2STR(event->event_info.sta_er_pin.pin_code));
-	break;
-    default:
-        break;
+
+static void event_handler(void *arg, esp_event_base_t event_base,
+	int32_t event_id, void *event_data)
+{
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+	esp_wifi_connect();
+    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+	if (s_retry_num < EXAMPLE_ESP_MAXIMUM_RETRY) {
+	    esp_wifi_connect();
+	    s_retry_num++;
+	    ESP_LOGI(TAG, "retry to connect to the AP");
+	} else {
+	    xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
+	}
+	ESP_LOGI(TAG, "connect to the AP fail");
+    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+	ip_event_got_ip_t *event = (ip_event_got_ip_t *)event_data;
+	ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
+	s_retry_num = 0;
+	xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
     }
-    return ESP_OK;
 }
 
 static void callback(struct netconn *conn, enum netconn_evt event, u16_t len)
@@ -525,27 +493,67 @@ static void wifi_init_sta(void)
 {
     s_wifi_event_group = xEventGroupCreate();
 
-    tcpip_adapter_init();
-    ESP_ERROR_CHECK(esp_event_loop_init(event_handler, NULL) );
+    ESP_ERROR_CHECK(esp_netif_init());
+
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_sta();
 
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
+
+    esp_event_handler_instance_t instance_any_id;
+    esp_event_handler_instance_t instance_got_ip;
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
+		ESP_EVENT_ANY_ID,
+		&event_handler,
+		NULL,
+		&instance_any_id));
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
+		IP_EVENT_STA_GOT_IP,
+		&event_handler,
+		NULL,
+		&instance_got_ip));
+
     ESP_ERROR_CHECK(esp_wifi_set_ps(WIFI_PS_NONE));
 
     wifi_config_t wifi_config = {
         .sta = {
             .ssid = EXAMPLE_ESP_WIFI_SSID,
             .password = EXAMPLE_ESP_WIFI_PASS,
+	    .threshold.authmode = WIFI_AUTH_WPA2_PSK,
+
+	    .pmf_cfg = {
+		.capable = true,
+		.required = false,
+	    },
         },
     };
-
-    ESP_LOGI(TAG, "wifi_init_sta finished.");
-    ESP_LOGI(TAG, "connect to ap SSID:%s password:%s",
-             EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS);
-
-    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config) );
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
     ESP_ERROR_CHECK(esp_wifi_start() );
+
+    //ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config) );
+    
+    ESP_LOGI(TAG, "wifi_init_sta finished.");
+
+    EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
+	    WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
+	    pdFALSE,
+	    pdFALSE,
+	    portMAX_DELAY);
+
+    if (bits & WIFI_CONNECTED_BIT) {
+	ESP_LOGI(TAG, "connect to ap SSID:%s password:%s",
+		EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS);
+    } else if (bits & WIFI_FAIL_BIT) {
+	ESP_LOGI(TAG, "Failed to connect to SSID:%s, password:%s",
+		EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS);
+    } else {
+	ESP_LOGE(TAG, "UNEXPECTED EVENT");
+    }
+
+    ESP_ERROR_CHECK(esp_event_handler_instance_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP, instance_got_ip));
+    ESP_ERROR_CHECK(esp_event_handler_instance_unregister(WIFI_EVENT, ESP_EVENT_ANY_ID, instance_any_id));
 
 #ifdef USE_WPS
     ESP_ERROR_CHECK(esp_wifi_start() );
